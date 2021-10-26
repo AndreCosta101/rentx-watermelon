7e66c8484dd2db8199a5acea22035118730c98bd
AndreCosta101 <andre.costa101@gmail.com>
2021-10-26 08:03:31 -0300

    Componente de Slider

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
 
 import {
   Container,
-  Header
+  Header,
+  CarImages
 } from './styles';
 
 export function CarDetails() {
@@ -14,6 +16,12 @@ export function CarDetails() {
           onPress={() => { }}
         />
       </Header>
+
+      <CarImages>
+        <ImageSlider
+          imagesUrl={['https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png']}
+        />
+      </CarImages>
     </Container>
   );
 }
