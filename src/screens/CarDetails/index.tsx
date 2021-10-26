614581e16b4e9de541c9c33bcfa2b70ac5e0d86c
AndreCosta101 <andre.costa101@gmail.com>
2021-10-26 11:08:43 -0300

    Componente de Acessórios

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
+import { Accessory } from '../../components/Accessory';
+
+import speedSvg from '../../assets/speed.svg';
+import accelerationSvg from '../../assets/acceleration.svg';
+import forceSvg from '../../assets/force.svg';
+import gasolineSvg from '../../assets/gasoline.svg';
+import gearSvg from '../../assets/gear.svg';
+import PeopleSvg from '../../assets/people.svg';
 
 import {
   Container,
@@ -14,6 +22,8 @@ import {
   Rent,
   Period,
   Price,
+  About,
+  Accessories
 } from './styles';
 
 export function CarDetails() {
@@ -43,8 +53,19 @@ export function CarDetails() {
           </Rent>
         </Details>
 
+        <Accessories>
+          <Accessory name="380Km/h" icon={speedSvg} />
+          <Accessory name="3.2s" icon={accelerationSvg} />
+          <Accessory name="800 HP" icon={forceSvg} />
+          <Accessory name="Gasolina" icon={gasolineSvg} />
+          <Accessory name="Auto" icon={gearSvg} />
+          <Accessory name="2 pessoas" icon={PeopleSvg} />
+        </Accessories>
+
         <About>
-          Este é automóvel desportivo. Surgiu do lendário touro de lide infultado na praça Real Maestranza de Sevilla. É um belíssimo carro para comer a mulherada.
+          Este é automóvel desportivo. Surgiu do lendário
+          touro de lide infultado na praça Real Maestranza
+          de Sevilla. É um belíssimo carro para comer a mulherada.
         </About>
       </Content>
     </Container>
