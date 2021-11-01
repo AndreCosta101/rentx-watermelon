9f4ffdf935e579b407d90ce8348fbb4327863f58
AndreCosta101 <andre.costa101@gmail.com>
2021-11-01 17:53:23 -0300

    Listando carros da API

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { useNavigation } from '@react-navigation/native';
 
 import { api } from '../../services/api';
+import { CarDTO } from '../../dtos/CarDTO';
 
 import Logo from '../../assets/logo.svg';
 import { Car } from '../../components/Car';
+import { Load } from '../../components/Load';
 
 import {
   Container,
@@ -17,7 +19,8 @@ import {
 } from './styles';
 
 export function Home() {
-
+  const [cars, setCars] = useState<CarDTO[]>([]);
+  const [loading, setLoading] = useState(true);
   const navigation = useNavigation<any>();
 
 
@@ -39,8 +42,14 @@ export function Home() {
 
   useEffect(() => {
     async function fetchCars() {
-      const response = await api.get('/cars')
-      console.log(response)
+      try {
+        const response = await api.get('/cars')
+        setCars(response.data)
+      } catch (error) {
+        console.log(error)
+      } finally {
+        setLoading(false)
+      }
     }
 
     fetchCars();
@@ -65,14 +74,14 @@ export function Home() {
         </HeaderContent>
       </Header>
 
-
-      <CarList
-        data={[1, 2, 3, 4, 5, 6, 7]}
-        keyExtractor={item => String(item)}
-        renderItem={({ item }) =>
-          <Car data={carData} onPress={handleCarDetails} />}
-
-      />
+      {loading ? <Load /> :
+        <CarList
+          data={cars}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) =>
+            <Car data={item} onPress={handleCarDetails} />}
+        />
+      }
     </Container>
   );
 }
