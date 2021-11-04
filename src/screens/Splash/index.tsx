6ccfe52c65f3342c63527b22d3c8e51b241f7442
AndreCosta101 <andre.costa101@gmail.com>
2021-11-04 17:43:46 -0300

    Curva de Bezier

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
-import { Button, StyleSheet } from 'react-native';
+import { Button, Dimensions, StyleSheet } from 'react-native';
 
-import Animated, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated';
+const WIDTH = Dimensions.get('window').width;
+
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  Easing
+} from 'react-native-reanimated';
 
 import {
   Container
@@ -13,14 +20,19 @@ export function Splash() {
   const animatedStyles = useAnimatedStyle(() => {
     return {
       transform: [
-        { translateX: animation.value }
+        {
+          translateX: withTiming(animation.value, {
+            duration: 500,
+            easing: Easing.bezier(0, 1.9, 1, -1.38)
+          })
+        }
       ]
     }
   })
 
 
   function handleAnimationPosition() {
-    animation.value = Math.random() * 100;
+    animation.value = Math.random() * (WIDTH - 100);
   }
 
 
