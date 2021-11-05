6ab3cc2fa068dca2cfc83510b913696892a03714
AndreCosta101 <andre.costa101@gmail.com>
2021-11-05 18:27:56 -0300

    Tela de Login

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -11,6 +11,7 @@ import { SchedulingDetails } from '../screens/SchedulingDetails';
 import { SchedulingComplete } from '../screens/SchedulingComplete';
 import { MyCars } from '../screens/MyCars';
 import { Splash } from '../screens/Splash';
+import { SignIn } from '../screens/SignIn';
 
 export function StackRoutes() {
   return (
@@ -20,8 +21,8 @@ export function StackRoutes() {
         headerShown: false,
       }}>
       <Screen
-        name="Splash"
-        component={Splash}
+        name="SignIn"
+        component={SignIn}
       />
       <Screen
         name="Home"
