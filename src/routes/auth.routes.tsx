a757c8539991f84427b09eb224cef4151b1d8304
AndreCosta101 <andre.costa101@gmail.com>
2021-11-08 17:43:55 -0300

    Rotas Privadas e Públicas

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -4,27 +4,29 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
-import { Home } from '../screens/Home';
-import { CarDetails } from '../screens/CarDetails';
-import { Scheduling } from '../screens/Scheduling';
-import { SchedulingDetails } from '../screens/SchedulingDetails';
 import { Confirmation } from '../screens/Confirmation';
-import { MyCars } from '../screens/MyCars';
 import { Splash } from '../screens/Splash';
 import { SignIn } from '../screens/SignIn';
 import { SignUpFirstStep } from '../screens/SignUp/SignUpFirstStep';
 import { SignUpSecondStep } from '../screens/SignUp/SignUpSecondStep';
 
-export function StackRoutes() {
+export function AuthRoutes() {
   return (
     <Navigator
-      initialRouteName="Home"
+      initialRouteName="Splash"
       screenOptions={{
         headerShown: false,
       }}>
       <Screen
+        name="Splash"
+        component={Splash}
+      />
+      <Screen
         name="SignIn"
         component={SignIn}
+        options={{
+          gestureEnabled: false,
+        }}
       />
       <Screen
         name="SignUpFirstStep"
@@ -35,32 +37,13 @@ export function StackRoutes() {
         component={SignUpSecondStep}
       />
       <Screen
-        name="Home"
-        component={Home}
+        name="Confirmation"
+        component={Confirmation}
         options={{
           gestureEnabled: false,
         }}
       />
-      <Screen
-        name="CarDetails"
-        component={CarDetails}
-      />
-      <Screen
-        name="Scheduling"
-        component={Scheduling}
-      />
-      <Screen
-        name="SchedulingDetails"
-        component={SchedulingDetails}
-      />
-      <Screen
-        name="Confirmation"
-        component={Confirmation}
-      />
-      <Screen
-        name="MyCars"
-        component={MyCars}
-      />
+
 
     </Navigator>
   )
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,20 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { StatusBar, StyleSheet, BackHandler } from 'react-native';
-import { useTheme } from 'styled-components';
+import { StatusBar, BackHandler } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { useNavigation } from '@react-navigation/native';
-import { Ionicons } from '@expo/vector-icons';
-import { RectButton, PanGestureHandler } from 'react-native-gesture-handler';
-
-import Animated, {
-  useSharedValue,
-  useAnimatedStyle,
-  useAnimatedGestureHandler,
-  withSpring
-} from 'react-native-reanimated';
-import { getBottomSpace } from 'react-native-iphone-x-helper';
-
-const ButtonAnimated = Animated.createAnimatedComponent(RectButton);
 
 import { api } from '../../services/api';
 import { CarDTO } from '../../dtos/CarDTO';
@@ -35,35 +22,6 @@ export function Home() {
   const [cars, setCars] = useState<CarDTO[]>([]);
   const [loading, setLoading] = useState(true);
   const navigation = useNavigation<any>();
-
-  const theme = useTheme();
-
-  const positionY = useSharedValue(0);
-  const positionX = useSharedValue(0);
-
-  const myCarsButtonStyle = useAnimatedStyle(() => {
-    return {
-      transform: [
-        { translateX: positionX.value },
-        { translateY: positionY.value },
-      ]
-    }
-  })
-
-  const onGestureEvent = useAnimatedGestureHandler({
-    onStart(event, ctx: any) {
-      ctx.positionX = positionX.value;
-      ctx.positionY = positionY.value;
-    },
-    onActive(event, ctx: any) {
-      positionX.value = ctx.positionX + event.translationX;
-      positionY.value = ctx.positionY + event.translationY;
-    },
-    onEnd() {
-      positionX.value = withSpring(0);
-      positionY.value = withSpring(0);
-    }
-  })
 
   function handleCarDetails(car: CarDTO) {
     navigation.navigate('CarDetails', { car });
@@ -127,42 +85,9 @@ export function Home() {
         />
       }
 
-      <PanGestureHandler onGestureEvent={onGestureEvent} >
-        <Animated.View
-          style={[
-            myCarsButtonStyle,
-            {
-              position: 'absolute',
-              bottom: getBottomSpace() + 13,
-              right: 22,
 
-            }
-          ]}
-        >
-          <ButtonAnimated
-            onPress={handleOpenMyCars}
-            style={[styles.button, { backgroundColor: theme.colors.main }]}
-          >
-            <Ionicons
-              name='ios-car-sport'
-              size={32}
-              color={theme.colors.shape}
-            />
-          </ButtonAnimated>
-        </Animated.View>
-      </PanGestureHandler>
 
 
     </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  button: {
-    width: 60,
-    height: 60,
-    borderRadius: 30,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
